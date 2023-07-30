87ff6476fc66921f126fddd077acd2c662841f76
Likithsreenivasa <118590713+Likithsreenivasa@users.noreply.github.com>
2023-07-30 15:43:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,12 +6,19 @@ function swapTheme(){
 	{
 		appDiv.classList.remove("day");
 		appDiv.classList.add("night");
+		button.classList.remove("button_day");
+		button.classList.add("button_night");
+		
 		
 	}
 	else
 	{
 		appDiv.classList.remove("night");
 		appDiv.classList.add("day");
+		button.classList.remove("button_night");
+		button.classList.add("button_day");
+		
+		
 	}
 	isDay=!isDay;
 }
