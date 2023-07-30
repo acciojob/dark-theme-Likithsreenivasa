b368ed4f6cd9428ffa6be540ffb5a51b94356a27
Likithsreenivasa <118590713+Likithsreenivasa@users.noreply.github.com>
2023-07-30 15:34:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-//your code here
+let isDay=true
+
+function swapTheme(){
+	let appDiv=document.getElementById("app");
+	if(isDay)
+	{
+		appDiv.classList.remove("day");
+		appDiv.classList.add("night");
+		
+	}
+	else
+	{
+		appDiv.classList.remove("night");
+		appDiv.classList.add("day");
+	}
+	isDay=!isDay;
+}
+const button=document.getElementById("swap");
+button.addEventListener("click",swapTheme);
+
+
 
